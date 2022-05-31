d0ca207b274cee3261b79c8441295b2efbc12194
shs <orolsyeo@gmail.com>
2022-05-31 10:15:56 +0900

    java object oriented study

---
+++ src/main/java/demo/JavaObjectOrientedApplication.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class JavaObjectOrientedApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(JavaObjectOrientedApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+	}
+}
