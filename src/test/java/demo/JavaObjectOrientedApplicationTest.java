e4a0be5ccb0916383aff55e56c68ccead1f051ff
shs <orolsyeo@gmail.com>
2022-05-31 10:40:18 +0900

    java object oriented study

---
+++ src/test/java/demo/JavaObjectOrientedApplicationTest.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class JavaObjectOrientedApplicationTest {
+
+	@Test
+	void contextLoads() {
+		
+	}
+}
